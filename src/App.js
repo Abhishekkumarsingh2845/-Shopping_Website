26b72ad69c2fabd6453a84be330a2b1f0270c332
Abhishekkumarsingh2845 <109286249+Abhishekkumarsingh2845@users.noreply.github.com>
2024-05-26 14:24:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="banner">
+      <div className='first'>
+        <div className='firstinner'>
+        <p className='firstText'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
+        </div>
+        <div className='secondinner'>
+        <p>ShopNow</p>
+        </div>
+      </div>
+      <div className='second'>
+        <div className='secondouter'>
+          <p className='text1'>English</p>
+          <img src={`./Asset/arrow.png`} alt="Logo" />
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
