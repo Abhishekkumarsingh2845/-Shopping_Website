252f109cf69938635663ef0daaabd711557594c8
Abhishekkumarsingh2845 <109286249+Abhishekkumarsingh2845@users.noreply.github.com>
2024-05-26 19:31:59 +0530

    banner has been created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import './App.css';
+import React from "react";
+import Banner from "./Components/Banner/Banner";
 
 function App() {
   return (
-    <div className="App">
-      <div className="banner">
-      <div className='first'>
-        <div className='firstinner'>
-        <p className='firstText'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
-        </div>
-        <div className='secondinner'>
-        <p>ShopNow</p>
-        </div>
-      </div>
-      <div className='second'>
-        <div className='secondouter'>
-          <p className='text1'>English</p>
-          <img src={`./Asset/arrow.png`} alt="Logo" />
-        </div>
-      </div>
-      </div>
+    <div>
+      <Banner />
     </div>
   );
 }
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,27 @@
+import './Banner.css';
+  
+
+function App() {
+  return (
+    <div className="App">
+      <div className="banner">
+      <div className='first'>
+        <div className='firstinner'>
+        <p className='firstText'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
+        </div>
+        <div className='secondinner'>
+        <p className='secondText'>ShopNow</p>
+        </div>
+      </div>
+      <div className='second'>
+       
+          <p className='text1'>English</p>
+          {/* <img src={ww} alt="Logo"  className='ima' /> */}
+      
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
